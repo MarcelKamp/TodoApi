9006a20f31f306d81c8bf65d1c7bd70323b1541b
MarcelKamp <MarcelKamp@hotmail.com>
2016-05-30 17:18:10 +0200

    Completed controller and routing

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todoRepository.GetAll();
+        }
+
+        [HttpGet("{id}", Name= "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = _todoRepository.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _todoRepository.Add(item);
+            return CreatedAtRoute("GetTodo", new {controller = "Todo", id = item.Key}, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = _todoRepository.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            _todoRepository.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _todoRepository.Remove(id);
+        }
+    }
+}
